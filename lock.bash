89762595da4f5639d46226d79fc98ee0556cc898
Nick Gilbert <nickgilbert2@gmail.com>
2017-05-16 10:49:01 -0700

    Adds an icon to the lock screen

--- lock.bash
+++ lock.bash
@@ -1,5 +1,12 @@
 #!/bin/bash
 scrot /tmp/screen.png
 convert /tmp/screen.png -scale 10% -scale 1000% /tmp/screen.png
+
+icon=$HOME/Pictures/affinty.png
+[[ -f $icon ]] && convert $icon -scale 50% /tmp/icon.png
+icon=/tmp/icon.png
+
+[[ -f $icon ]] && convert /tmp/screen.png $icon -gravity center -composite -matte /tmp/screen.png
+
 i3lock -i /tmp/screen.png
 rm /tmp/screen.png
