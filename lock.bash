23e5acda73c8f3fcc127578d2ed7cfce1f17afa2
Nick Gilbert <nickgilbert2@gmail.com>
2017-05-16 10:51:17 -0700

    Makes logo passed in rather than hard-coded

--- lock.bash
+++ lock.bash
@@ -2,11 +2,7 @@
 scrot /tmp/screen.png
 convert /tmp/screen.png -scale 10% -scale 1000% /tmp/screen.png
 
-icon=$HOME/Pictures/affinty.png
-[[ -f $icon ]] && convert $icon -scale 50% /tmp/icon.png
-icon=/tmp/icon.png
-
-[[ -f $icon ]] && convert /tmp/screen.png $icon -gravity center -composite -matte /tmp/screen.png
+[[ -f $1 ]] && convert /tmp/screen.png $1 -gravity center -composite -matte /tmp/screen.png
 
 i3lock -i /tmp/screen.png
 rm /tmp/screen.png
