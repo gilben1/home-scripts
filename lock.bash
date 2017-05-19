8da62cf772d138f29f68f6fda4eb3b863bade872
Nick Gilbert <nickgilbert2@gmail.com>
2017-05-18 17:56:14 -0700

    Moves lock icon to be ever so slightly centered

--- lock.bash
+++ lock.bash
@@ -4,10 +4,11 @@ convert /tmp/screen.png -scale 10% -scale 1000% /tmp/screen.png
 
 # Grabs a random icon from the folder ~/.lock_icons
 icon=$(ls ~/.lock_icons | shuf -n 1)
+
 # Adds the path to it
 icon="$HOME/.lock_icons/$icon"
 
-[[ -f $icon ]] && convert /tmp/screen.png $icon -gravity center -composite /tmp/screen.png
+[[ -f $icon ]] && convert /tmp/screen.png $icon -gravity center -geometry -2-1 -composite /tmp/screen.png
 
 i3lock -i /tmp/screen.png
 rm /tmp/screen.png
