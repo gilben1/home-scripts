c1c557b845955946fc5da892c0815d31fbdc7585
Nick Gilbert <nickgilbert2@gmail.com>
2017-05-24 10:57:51 -0700

    Modifies to work with any number of monitors

--- lock.bash
+++ lock.bash
@@ -1,30 +1,40 @@
 #!/bin/bash
 
-# Script modified by Nick Gilbert
-# Original script found here: https://github.com/savoca/dotfiles/blob/gray/home/.bin/scripts/lock.sh
-# Use and modify as you will
-
-# Locks the screen with a pixellated screenshot of the screen right before lock
-# Adds an optional icon grabbed randomly from the ~/.lock_icons directory
-# Note, all icons look best at 160 x 160 on a 1366 x 768 screen
-#   some experimentation may be required to make the icon look good
-
-# Screenshot the screen
 scrot /tmp/screen.png
-# Scale down and back up for pixellated effect
 convert /tmp/screen.png -scale 10% -scale 1000% /tmp/screen.png
-
 # Grabs a random icon from the folder ~/.lock_icons
 icon=$(ls ~/.lock_icons | shuf -n 1)
 
 # Adds the path to it
 icon="$HOME/.lock_icons/$icon"
 
-# If the icon exists, add it to pretty much the center
-[[ -f $icon ]] && convert /tmp/screen.png $icon -gravity center -geometry -2-1 -composite /tmp/screen.png
 
-# Run i3lock with our generated screenshot
+if [[ -f $icon ]]
+then
+    # placement x/y
+    PX=0
+    PY=0
+    # lockscreen image info
+    R=$(file $icon | grep -o '[0-9]* x [0-9]*')
+    RX=$(echo $R | cut -d' ' -f 1)
+    RY=$(echo $R | cut -d' ' -f 3)
+
+    SR=$(xrandr --query | grep ' connected' | cut -f3 -d' ')
+    for RES in $SR
+    do
+        # monitor position/offset
+        SRX=$(echo $RES | cut -d'x' -f 1)                   # x pos
+        SRY=$(echo $RES | cut -d'x' -f 2 | cut -d'+' -f 1)  # y pos
+        SROX=$(echo $RES | cut -d'x' -f 2 | cut -d'+' -f 2) # x offset
+        SROY=$(echo $RES | cut -d'x' -f 2 | cut -d'+' -f 3) # y offset
+        PX=$(($SROX + $SRX/2 - $RX/2))
+        PY=$(($SROY + $SRY/2 - $RY/2))
+
+        convert /tmp/screen.png $icon -geometry +$PX+$PY -composite -matte  /tmp/screen.png
+        echo "done"
+    done
+fi
+# dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Stop
+# i3lock  -I 10 -d -e -u -n -i /tmp/screen.png
+#i3lock -e -u -n -i /tmp/screen.png
 i3lock -i /tmp/screen.png
-
-# remove when done
-rm /tmp/screen.png
