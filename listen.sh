8905d0e734665745f7f9f9faebc6978cfbd3d4e3
Nick Gilbert <nickgilbert2@gmail.com>
2017-05-26 13:06:55 -0700

    Small change to notify send bit of listen.sh

--- listen.sh
+++ listen.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Notification thing written by grpyhon. Change to your $user and $host rather than mine if you are using it
-ssh -4 nickg@serenity.cat.pdx.edu "tail -n0 -f ~/beep.log" </dev/null | nohup ruby -rshellwords -ne '`notify-send -t 1000 #{$_.shellescape}`' >/dev/null 2>&1
+ssh -4 nickg@serenity.cat.pdx.edu "tail -n0 -f ~/beep.log" </dev/null | nohup ruby -rshellwords -ne '`notify-send #{$_.shellescape}`' >/dev/null 2>&1 &
