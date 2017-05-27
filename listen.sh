e579f4cdb74da692046cd5e874101895d9e29f05
Nick Gilbert <nickgilbert2@gmail.com>
2017-05-26 20:34:31 -0700

    Adds support for weechat icon in notification

--- listen.sh
+++ listen.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Notification thing written by grpyhon. Change to your $user and $host rather than mine if you are using it
-ssh -4 nickg@serenity.cat.pdx.edu "tail -n0 -f ~/beep.log" </dev/null | nohup ruby -rshellwords -ne '`notify-send #{$_.shellescape}`' >/dev/null 2>&1 &
+ssh -4 nickg@serenity.cat.pdx.edu "tail -n0 -f ~/beep.log" </dev/null | nohup ruby -rshellwords -ne '`notify-send #{$_.shellescape} -i $HOME/.notify_icons/weechat.png`' >/dev/null 2>&1 &
