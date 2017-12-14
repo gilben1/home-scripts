2f507cb98cec04a7fa446a56e3da222cb6bc82df
Nick Gilbert <nickgilbert2@gmail.com>
2017-12-13 20:08:57 -0800

    Changed rofi.bash to rofi-run, added script for grabbing bash aliases

---
+++ alias.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+alias | awk -F'[ =]' '{print $2}'
--- rofi.bash
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Script by Nick Gilbert (nickgilbert2@gmail.com)
-# Use and modify as you will
-
-# Run rofi application launcher with a set of customization options
-
-rofi -show run -modi run,window,drun -opacity 90 -fg "#000000" -hlbg "#5bb341" -sidebar-mode true -font "Consolas 10" -disable-history -separator-style "solid"
