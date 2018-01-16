42277d3dddc1d3e50e6586e5b7befad730c0f1ab
Nick Gilbert <nickgilbert2@gmail.com>
2018-01-16 11:34:42 -0800

    (rofi-run, alias.sh) Refined alias.sh, uses in new rofi-run to highlight
    both functions and aliases found in the run tab of rofi-run

--- alias.sh
+++ alias.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-alias | awk -F'[ =]' '{print $2}'
+# Outputs the aliases and functions defined in .bash_aliases
+
+aliases=$(cat ~/.bash_aliases | grep ^alias | cut -d ' ' -f 2 | cut -d '=' -f 1)
+functions=$(cat ~/.bash_aliases | grep ^function | cut -d ' ' -f 2 | rev | cut -c 3- | rev)
+
+echo "${aliases}${functions}"
