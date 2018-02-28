2b2ac59161741004bb2bf9cd33c478c9b323c821
Nick Gilbert <nickgilbert2@gmail.com>
2018-02-27 18:41:26 -0800

    (rename.bash) Changed prompt from zenity to rofi

--- rename.bash
+++ rename.bash
@@ -6,7 +6,9 @@
 #  https://faq.i3wm.org/question/3936/how-can-i-change-the-position-of-i3-input-prompt/
 #
 WSNUMBER=$(ruby -rjson -e 'puts(JSON.parse(`i3-msg -t get_workspaces`).find { |ws| ws["focused"] }["num"])')
-NEWNAME="`zenity --entry --text 'Rename workspace to: '`"
+NEWNAME=$(echo | rofi   -dmenu \
+                        -i \
+                        -p "Workspace Name: ")
 if [ -z "${NEWNAME}" ] ; then
     NEWNAME="${WSNUMBER}"
 elif [[ ! "${NEWNAME}" =~ ^[0-9]+: ]] ; then
