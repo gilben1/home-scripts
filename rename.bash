77758b08354b2ff39c8c6c15468f5bbe50eb466b
Nick Gilbert <nickgilbert2@gmail.com>
2018-03-13 10:04:49 -0700

    (rename.bash) Shows workspace name and number in rofi prompt

--- rename.bash
+++ rename.bash
@@ -6,15 +6,16 @@
 #  https://faq.i3wm.org/question/3936/how-can-i-change-the-position-of-i3-input-prompt/
 #
 WSNUMBER=$(ruby -rjson -e 'puts(JSON.parse(`i3-msg -t get_workspaces`).find { |ws| ws["focused"] }["num"])')
+OLDNAME=$(ruby -rjson -e 'puts(JSON.parse(`i3-msg -t get_workspaces`).find { |ws| ws["focused"] }["name"])')
 NEWNAME=$(echo | rofi   -dmenu \
                         -i \
                         -hide-scrollbar true \
                         -width 35 \
                         -location 7 \
                         -lines 0 \
-                        -p "Workspace Name for (${WSNUMBER})")
+                        -p "Workspace Name for (${OLDNAME})")
 if [ -z "${NEWNAME}" ] ; then
-    NEWNAME="${WSNUMBER}"
+    NEWNAME="${OLDNAME}"
 elif [[ ! "${NEWNAME}" =~ ^[0-9]+: ]] ; then
     NEWNAME="${WSNUMBER}:${NEWNAME}"
 fi
